580d272f86334a9aaa872b584bf0f7da88c0dc4d
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-04-03 16:00:21 +0530

    Result display check

--- webapp.py
+++ webapp.py
@@ -43,56 +43,143 @@ def hello_world():
     return render_template("index.html")
 
 
+# @app.route("/", methods=["GET", "POST"])
+# def predict_img():
+#     if request.method == "POST":
+#         if "file" in request.files:
+#             f = request.files["file"]
+#             basepath = os.path.dirname(__file__)
+#             filepath = os.path.join(basepath, "uploads", f.filename)
+#             print("upload folder is ", filepath)
+#             f.save(filepath)
+#             predict_img.imgpath = f.filename
+#             print("printing predict_img :::::: ", predict_img)
+
+#             file_extension = f.filename.rsplit(".", 1)[1].lower()
+
+#             if file_extension == "jpg":
+#                 img = cv2.imread(filepath)
+
+#                 # Perform the detection
+#                 model = YOLO("yolov8n.pt")
+#                 detections = model(img, save=True)
+
+#                 # Find the latest subdirectory in the 'runs/detect' folder
+#                 folder_path = os.path.join(basepath, "runs", "detect")
+#                 subfolders = [
+#                     f
+#                     for f in os.listdir(folder_path)
+#                     if os.path.isdir(os.path.join(folder_path, f))
+#                 ]
+#                 latest_subfolder = max(
+#                     subfolders,
+#                     key=lambda x: os.path.getctime(os.path.join(folder_path, x)),
+#                 )
+
+#                 # Construct the relative path to the detected image file
+#                 static_folder = os.path.join(basepath, "static", "assets")
+#                 relative_image_path = os.path.relpath(
+#                     os.path.join(folder_path, latest_subfolder, f.filename),
+#                     static_folder,
+#                 )
+#                 image_path = os.path.join(folder_path, latest_subfolder, f.filename)
+#                 print(
+#                     "Relative image path:", relative_image_path
+#                 )  # Print the relative_image_path for debugging
+
+#                 # return render_template('index.html', image_path=image_path)
+#                 return render_template(
+#                     "index.html", image_path=relative_image_path, media_type="image"
+#                 )
+
+#             elif file_extension == "mp4":
+#                 video_path = filepath  # replace with your video path
+#                 cap = cv2.VideoCapture(video_path)
+
+#                 # get video dimensions
+#                 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+#                 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+#                 # Define the codec and create VideoWriter object
+#                 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+#                 out = cv2.VideoWriter(
+#                     "output.mp4", fourcc, 30.0, (frame_width, frame_height)
+#                 )
+
+#                 # initialize the YOLOv8 model here
+#                 model = YOLO("yolov8n.pt")
+
+#                 while cap.isOpened():
+#                     ret, frame = cap.read()
+#                     if not ret:
+#                         break
+
+#                     results = model(frame, save=True)  # working
+#                     print(results)
+#                     cv2.waitKey(1)
+
+#                     res_plotted = results[0].plot()
+#                     cv2.imshow("result", res_plotted)
+
+#                     # write the frame to the output video
+#                     out.write(res_plotted)
+
+#                     if cv2.waitKey(1) == ord("q"):
+#                         break
+
+#                 return video_feed()
+
+#     folder_path = "runs/detect"
+#     subfolders = [
+#         f
+#         for f in os.listdir(folder_path)
+#         if os.path.isdir(os.path.join(folder_path, f))
+#     ]
+#     latest_subfolder = max(
+#         subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x))
+#     )
+#     image_path = folder_path + "/" + latest_subfolder + "/" + f.filename
+#     return render_template("index.html", image_path=image_path)
+#     # return "done"
+
+
 @app.route("/", methods=["GET", "POST"])
 def predict_img():
     if request.method == "POST":
-        if "file" in request.files:
-            f = request.files["file"]
+        if 'file' in request.files:
+            f = request.files['file']
             basepath = os.path.dirname(__file__)
-            filepath = os.path.join(basepath, "uploads", f.filename)
+            filepath = os.path.join(basepath, 'uploads', f.filename)
             print("upload folder is ", filepath)
             f.save(filepath)
             predict_img.imgpath = f.filename
             print("printing predict_img :::::: ", predict_img)
 
-            file_extension = f.filename.rsplit(".", 1)[1].lower()
+            file_extension = f.filename.rsplit('.', 1)[1].lower()
 
-            if file_extension == "jpg":
+            if file_extension == 'jpg':
+                # Handle image upload
                 img = cv2.imread(filepath)
 
                 # Perform the detection
-                model = YOLO("yolov8n.pt")
+                model = YOLO('yolov8n.pt')
                 detections = model(img, save=True)
 
                 # Find the latest subdirectory in the 'runs/detect' folder
-                folder_path = os.path.join(basepath, "runs", "detect")
-                subfolders = [
-                    f
-                    for f in os.listdir(folder_path)
-                    if os.path.isdir(os.path.join(folder_path, f))
-                ]
-                latest_subfolder = max(
-                    subfolders,
-                    key=lambda x: os.path.getctime(os.path.join(folder_path, x)),
-                )
+                folder_path = os.path.join(basepath, 'runs', 'detect')
+                subfolders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]
+                latest_subfolder = max(subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x)))
 
                 # Construct the relative path to the detected image file
-                static_folder = os.path.join(basepath, "static", "assets")
-                relative_image_path = os.path.relpath(
-                    os.path.join(folder_path, latest_subfolder, f.filename),
-                    static_folder,
-                )
+                static_folder = os.path.join(basepath, 'static', 'assets')
+                relative_image_path = os.path.relpath(os.path.join(folder_path, latest_subfolder, f.filename), static_folder)
                 image_path = os.path.join(folder_path, latest_subfolder, f.filename)
-                print(
-                    "Relative image path:", relative_image_path
-                )  # Print the relative_image_path for debugging
-
-                # return render_template('index.html', image_path=image_path)
-                return render_template(
-                    "index.html", image_path=relative_image_path, media_type="image"
-                )
+                print("Relative image path:", relative_image_path)  # Print the relative_image_path for debugging
+                
+                return render_template('index.html', image_path=relative_image_path, media_type='image')
 
             elif file_extension == "mp4":
+                # Handle video upload
                 video_path = filepath  # replace with your video path
                 cap = cv2.VideoCapture(video_path)
 
@@ -114,6 +201,8 @@ def predict_img():
                     if not ret:
                         break
 
+                    # do YOLOv9 detection on the frame here
+                    # model = YOLO('yolov9c.pt')
                     results = model(frame, save=True)  # working
                     print(results)
                     cv2.waitKey(1)
@@ -127,20 +216,11 @@ def predict_img():
                     if cv2.waitKey(1) == ord("q"):
                         break
 
-                return video_feed()
+                return render_template('index.html', video_path='output.mp4', media_type='video')
 
-    folder_path = "runs/detect"
-    subfolders = [
-        f
-        for f in os.listdir(folder_path)
-        if os.path.isdir(os.path.join(folder_path, f))
-    ]
-    latest_subfolder = max(
-        subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x))
-    )
-    image_path = folder_path + "/" + latest_subfolder + "/" + f.filename
-    return render_template("index.html", image_path=image_path)
-    # return "done"
+    # If no file uploaded or GET request, return the template with default values
+    return render_template("index.html", image_path="", media_type='image')
+
 
 
 @app.route("/<path:filename>")
@@ -173,9 +253,37 @@ def display(filename):
         return "Invalid file format"
 
 
+# def get_frame():
+#     folder_path = os.getcwd()
+#     mp4_files = "output.mp4"
+#     video = cv2.VideoCapture(mp4_files)  # detected video path
+#     while True:
+#         success, image = video.read()
+#         if not success:
+#             break
+#         ret, jpeg = cv2.imencode(".jpg", image)
+
+#         yield (
+#             b"--frame\r\n"
+#             b"Content-Type: image/jpeg\r\n\r\n" + jpeg.tobytes() + b"\r\n\r\n"
+#         )
+#         time.sleep(
+#             0.1
+#         )  # Control the frame rate to display one frame every 100 milliseconds:
+
+
+# # function to display the detected objects video on html page
+# @app.route("/video_feed")
+# def video_feed():
+#     # print("function called")
+
+#     # return Response(get_frame(), mimetype="multipart/x-mixed-replace; boundary=frame")
+#     return Response(get_frame(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
 def get_frame():
     folder_path = os.getcwd()
     mp4_files = "output.mp4"
+    print("files being read...")
     video = cv2.VideoCapture(mp4_files)  # detected video path
     while True:
         success, image = video.read()
@@ -187,23 +295,19 @@ def get_frame():
             b"--frame\r\n"
             b"Content-Type: image/jpeg\r\n\r\n" + jpeg.tobytes() + b"\r\n\r\n"
         )
-        time.sleep(
-            0.1
-        )  # Control the frame rate to display one frame every 100 milliseconds:
+        time.sleep(0.1)  # Control the frame rate to display one frame every 100 milliseconds:
+
 
 
 # function to display the detected objects video on html page
 @app.route("/video_feed")
 def video_feed():
-    # print("function called")
-
-    # return Response(get_frame(), mimetype="multipart/x-mixed-replace; boundary=frame")
     return Response(get_frame(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 @app.route("/webcam_feed")
 def webcam_feed():
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0) # 0 for camera
 
     def generate():
         while True:

e9e0a25ee33bb7c6290d7e8d568300c81f53edc0
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-04-03 08:33:46 +0530

    Format Code (Indentation)

--- webapp.py
+++ webapp.py
@@ -1,4 +1,3 @@
-
 import argparse
 import io
 from PIL import Image
@@ -9,7 +8,15 @@ import cv2
 import numpy as np
 import tensorflow as tf
 from re import DEBUG, sub
-from flask import Flask, render_template, request, redirect, send_file, url_for, Response
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    send_file,
+    url_for,
+    Response,
+)
 from werkzeug.utils import secure_filename, send_from_directory
 import os
 import subprocess
@@ -29,120 +36,135 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html")
 
-    
+
 @app.route("/", methods=["GET", "POST"])
 def predict_img():
     if request.method == "POST":
-        if 'file' in request.files:
-            f = request.files['file']
+        if "file" in request.files:
+            f = request.files["file"]
             basepath = os.path.dirname(__file__)
-            filepath = os.path.join(basepath,'uploads',f.filename)
+            filepath = os.path.join(basepath, "uploads", f.filename)
             print("upload folder is ", filepath)
             f.save(filepath)
             global imgpath
             predict_img.imgpath = f.filename
             print("printing predict_img :::::: ", predict_img)
-                                               
-            file_extension = f.filename.rsplit('.', 1)[1].lower() 
-            
-            if file_extension == 'jpg':
+
+            file_extension = f.filename.rsplit(".", 1)[1].lower()
+
+            if file_extension == "jpg":
                 img = cv2.imread(filepath)
 
                 # Perform the detection
-                model = YOLO('yolov8n.pt')
-                detections =  model(img, save=True) 
+                model = YOLO("yolov8n.pt")
+                detections = model(img, save=True)
                 return display(f.filename)
-            
-            elif file_extension == 'mp4': 
+
+            elif file_extension == "mp4":
                 video_path = filepath  # replace with your video path
                 cap = cv2.VideoCapture(video_path)
 
                 # get video dimensions
                 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                            
+
                 # Define the codec and create VideoWriter object
-                fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-                out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (frame_width, frame_height))
-                
+                fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+                out = cv2.VideoWriter(
+                    "output.mp4", fourcc, 30.0, (frame_width, frame_height)
+                )
+
                 # initialize the YOLOv8 model here
-                model = YOLO('yolov8n.pt')
-                
+                model = YOLO("yolov8n.pt")
+
                 while cap.isOpened():
                     ret, frame = cap.read()
                     if not ret:
-                        break                                                      
+                        break
 
                     # do YOLOv9 detection on the frame here
-                    #model = YOLO('yolov9c.pt')
-                    results = model(frame, save=True)  #working
+                    # model = YOLO('yolov9c.pt')
+                    results = model(frame, save=True)  # working
                     print(results)
                     cv2.waitKey(1)
 
                     res_plotted = results[0].plot()
                     cv2.imshow("result", res_plotted)
-                    
+
                     # write the frame to the output video
                     out.write(res_plotted)
 
-                    if cv2.waitKey(1) == ord('q'):
+                    if cv2.waitKey(1) == ord("q"):
                         break
 
-                return video_feed()            
+                return video_feed()
 
-
-            
-    folder_path = 'runs/detect'
-    subfolders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]    
-    latest_subfolder = max(subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x)))    
-    image_path = folder_path+'/'+latest_subfolder+'/'+f.filename 
-    return render_template('index.html', image_path=image_path)
-    #return "done"
-
+    folder_path = "runs/detect"
+    subfolders = [
+        f
+        for f in os.listdir(folder_path)
+        if os.path.isdir(os.path.join(folder_path, f))
+    ]
+    latest_subfolder = max(
+        subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x))
+    )
+    image_path = folder_path + "/" + latest_subfolder + "/" + f.filename
+    return render_template("index.html", image_path=image_path)
+    # return "done"
 
 
 # #The display function is used to serve the image or video from the folder_path directory.
-@app.route('/<path:filename>')
+@app.route("/<path:filename>")
 def display(filename):
-    folder_path = 'runs/detect'
-    subfolders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]    
-    latest_subfolder = max(subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x)))    
-    directory = folder_path+'/'+latest_subfolder    
-    print("printing directory: ",directory) 
+    folder_path = "runs/detect"
+    subfolders = [
+        f
+        for f in os.listdir(folder_path)
+        if os.path.isdir(os.path.join(folder_path, f))
+    ]
+    latest_subfolder = max(
+        subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x))
+    )
+    directory = folder_path + "/" + latest_subfolder
+    print("printing directory: ", directory)
     files = os.listdir(directory)
     latest_file = files[0]
-    
+
     print(latest_file)
 
     filename = os.path.join(folder_path, latest_subfolder, latest_file)
 
-    file_extension = filename.rsplit('.', 1)[1].lower()
+    file_extension = filename.rsplit(".", 1)[1].lower()
 
     environ = request.environ
-    if file_extension == 'jpg':      
-        return send_from_directory(directory,latest_file,environ) #shows the result in seperate tab
+    if file_extension == "jpg":
+        return send_from_directory(
+            directory, latest_file, environ
+        )  # shows the result in seperate tab
 
     else:
         return "Invalid file format"
-        
-        
-        
+
 
 def get_frame():
     folder_path = os.getcwd()
-    mp4_files = 'output.mp4'
+    mp4_files = "output.mp4"
     video = cv2.VideoCapture(mp4_files)  # detected video path
     while True:
         success, image = video.read()
         if not success:
             break
-        ret, jpeg = cv2.imencode('.jpg', image) 
-      
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')   
-        time.sleep(0.1) # Control the frame rate to display one frame every 100 milliseconds: 
+        ret, jpeg = cv2.imencode(".jpg", image)
+
+        yield (
+            b"--frame\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n" + jpeg.tobytes() + b"\r\n\r\n"
+        )
+        time.sleep(
+            0.1
+        )  # Control the frame rate to display one frame every 100 milliseconds:
 
 
 # function to display the detected objects video on html page
@@ -150,48 +172,8 @@ def get_frame():
 def video_feed():
     print("function called")
 
-    return Response(get_frame(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(get_frame(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
-
-# @app.route("/webcam_feed")
-# def webcam_feed():
-#     cap = cv2.VideoCapture(0)
-
-#     def generate():
-#         while True:
-#             success, frame = cap.read()
-#             if not success:
-#                 break
-#             ret, buffer = cv2.imencode('.jpg', frame)
-#             frame =  buffer.tobytes()
-#             print(type(frame))
-
-#             img = Image.open(io.BytesIO(frame))
-
-#             model = YOLO('yolov8n.pt')
-#             results = model(img, save=True)
-
-#             print(results)
-#             cv2.waitKey(1)
-
-#             res_plotted = results[0].plot()
-#             cv2.imshow("result", res_plotted)
-
-#             if cv2.waitKey(1) == ord("q"):
-#                 break
-
-#             img_BGR = cv2.cvtColor(res_plotted, cv2.COLOR_RGB2BGR)
-
-
-#             frame = cv2.imencode('jpg', img_BGR)[1].tobytes()
-
-            
-
-#             yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n') 
-            
-#     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/webcam_feed")
 def webcam_feed():
@@ -205,7 +187,7 @@ def webcam_feed():
 
             # Perform object detection on the frame
             img = Image.fromarray(frame)
-            model = YOLO('yolov8n.pt')
+            model = YOLO("yolov8n.pt")
             results = model(img, save=True)
 
             # Plot the detected objects on the frame
@@ -213,22 +195,19 @@ def webcam_feed():
             img_BGR = cv2.cvtColor(res_plotted, cv2.COLOR_RGB2BGR)
 
             # Convert the frame to JPEG format for streaming
-            ret, buffer = cv2.imencode('.jpg', img_BGR)
+            ret, buffer = cv2.imencode(".jpg", img_BGR)
             frame = buffer.tobytes()
 
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n') 
+            yield (
+                b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n"
+            )
 
-    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-        
-        
+    return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask app exposing yolov8 models")
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
-    model = YOLO('yolov8n.pt')
-    app.run(host="0.0.0.0", port=args.port, debug=True) 
+    model = YOLO("yolov8n.pt")
+    app.run(host="0.0.0.0", port=args.port, debug=True)
